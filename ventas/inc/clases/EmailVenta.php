e21fd021ed2805e64de46794a19f73212803ef46
Thiegui <thiegui@gmail.com>
2023-02-15 18:09:56 +0100

    Update EmailVenta.php
    Cambio email petición Claudia ticket:
    https://jyctel.freshdesk.com/a/tickets/351

--- ventas/inc/clases/EmailVenta.php
+++ ventas/inc/clases/EmailVenta.php
@@ -8,10 +8,13 @@ class EmailVenta{
 	protected $hay_efectivo = '';
 	protected $token = '';
 
-	function __construct( $id_comercial ){
+	function __construct( $id_comercial )
+	{
 		$this->id_comercial = $id_comercial;
 	}
-	function getEmails ($tipo_email='') {
+
+	function getEmails($tipo_email='') 
+	{
 		$email = '';
 		if (isDevel()) {
 			$email = 'diego@jyctel.com';
@@ -27,10 +30,121 @@ class EmailVenta{
 		}
 		return $email;
 	}
-	function getTable(){
+
+	function getTable()
+	{
 		return $this->info;
 	}
-	function setData( $data ){
+
+	public function send()
+	{
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+		$headers .= 'From: <ventas@jyctel.com>' . "\r\n";
+		$headers .= 'Cc: diego@jyctel.com' . "\r\n";
+		
+		mail( $this->getEmails(), 'Resumen Venta - '.$this->token . $this->hay_efectivo, $this->info,$headers);
+	}
+
+	public function sendEmailCoche($info) 
+	{
+		$f_venta = $info['producto']['fecha_venta'];
+		$f_entrega = $info['producto']['fecha_inicial'];
+		$f_recogida = $info['producto']['fecha_final'];
+		$c_dias = $info['producto']['cantidad_dias'];
+		$loc = $info['producto']['localizador'];
+		$telefono = $info['cliente']['telefono'];
+		$contacto = $info['cliente']['contacto'];
+		$nombre = $info['cliente']['nombre'];
+
+		$token = $this->token;
+		//debug($info, $token);
+
+		$info = '<style>
+			table{text-align:left;width:fit-content;border:1px solid #03111f;}
+		        table tr{ border:1px solid gray;}
+		        table td, table th{padding:0px 10px; border: 1px solid gray;}
+			</style>';
+		$info .= '<table>
+			<tbody>
+			<tr><td style="text-align: center;" colspan="2"><strong>Info Coche</strong></td></tr>
+			<tr><td><strong>Localizador</strong></td><td>'.$loc.'</td></tr>
+			<tr><td><strong>Fecha Venta</strong></td><td>'.$f_venta.'</td></tr>
+			<tr><td><strong>Recoge Coche</strong></td><td>'.$f_entrega.'</td></tr>
+			<tr><td><strong>Entrega coche</strong></td><td>'.$f_recogida.'</td></tr>
+			<tr><td><strong>Cantidad d&iacute;as</strong></td><td>'.$c_dias.'</td></tr>
+			<tr><td><strong>Telefono</strong></td><td>'.$telefono.'</td></tr>
+			<tr><td><strong>Contacto</strong></td><td>'.$contacto.'</td></tr>
+			<tr><td><strong>Nombre</strong></td><td>'.$nombre.'</td></tr>
+			</tbody>
+			</table>';
+		$this->info = $info;
+	
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+		$headers .= 'From: <ventas@jyctel.com>' . "\r\n";
+		$headers .= 'Cc: diego@jyctel.com' . "\r\n";
+		
+		mail( $this->getEmails ('coche'), 'Resumen Venta Coche - '.$this->token, $this->info,$headers);
+	}
+
+	public function sendEmailTrustPilot($venta, $producto, $asignaciones_coche) 
+	{
+		$fecha_pago = date('Y-m-d');
+		foreach ($producto as $bloque) {
+			if (isset($bloque['producto'])) {
+				$fecha_pago = $bloque['producto']['fecha_venta'];
+			}
+			if (isset($bloque['facturacion'])) {
+				$cliente = $bloque['facturacion'];
+			} else{
+				$cliente = $bloque['cliente'];
+			}
+		}
+		
+		$cantidad = 0;	
+		foreach ($asignaciones_coche as $vals) {
+			$divisa = ($vals['divisa'] == 1 ? '&euro;' : '$');
+			$cantidad += $vals['cantidad'];
+		}	
+
+		$iNombre = explode(' ', $cliente['nombre']);
+		$nombre = ucfirst($iNombre[0]);
+		$nombre_completo = ucfirst($cliente['nombre']);
+
+		$vars_plantilla = array(
+			'[nombre]'=> $nombre,
+			'[nombre-completo]'=> $nombre_completo,
+			'[fecha-pago]' => $fecha_pago,
+			'[precio]' => number_format( $cantidad, 2, ',' , '') . ' ' . $divisa,
+		);
+		
+		$archivo_ori = file_get_contents('/var/www/html/factura/contratos-ventas/plantilla-cubarentcars/index.html');
+		$archivo = $archivo_ori;
+		foreach ($vars_plantilla as $tag => $value) {
+
+	 	       $archivo = str_replace( $tag, $value, $archivo);
+		}
+
+		$email_trustpilot = 'cubarentcars.com+399c370bbc@invite.trustpilot.com';		
+	
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+		$headers .= 'From: <ventas@jyctel.com>' . "\r\n";
+		$headers .= 'Bcc: '.$email_trustpilot . "\r\n";
+
+		$subject = 'Confirmacion de pago';
+		$email_cliente = $cliente['email'];	
+		//$email_cliente = 'thiegui@yahoo.es';	
+		
+		$res = mail($email_cliente, $subject, $archivo, $headers);
+
+		syslog (LOG_INFO, __FILE__ . ':'.__METHOD__ . ':cantidad: ' . $cantidad);
+		syslog (LOG_INFO, __FILE__ . ':'.__METHOD__ . ':' . ($res ? 'sended -- '.$email_cliente:'not-sended ' . $email_cliente));
+	}
+
+	function setData( $data ) 
+	{
 		$hay_efectivo = '';
 		$this->data = $data;
 		$this->token = $this->data['token_venta'];
@@ -92,6 +206,7 @@ class EmailVenta{
 					$info .= "<tr><th>Texto Comprobante</th></tr>";
 					$info .= sprintf( "<tr><th>%s</th></tr>", $asignacion[ 'texto' ] );
 				}
+
 			}
 		}
 		$info .= "</table>";
@@ -100,116 +215,8 @@ class EmailVenta{
 		$this->info = $info;
 	}
 
-	function setToken($token) {
-
+	function setToken($token) 
+	{
 		$this->token = $token;
-
-	}
-
-	function sendEmailCoche($info) {
-
-		$f_venta = $info['producto']['fecha_venta'];
-		$f_entrega = $info['producto']['fecha_inicial'];
-		$f_recogida = $info['producto']['fecha_final'];
-		$c_dias = $info['producto']['cantidad_dias'];
-		$loc = $info['producto']['localizador'];
-		$telefono = $info['cliente']['telefono'];
-		$contacto = $info['cliente']['contacto'];
-		$nombre = $info['cliente']['nombre'];
-
-		$token = $this->token;
-		//debug($info, $token);
-
-		$info = '<style>
-			table{text-align:left;width:fit-content;border:1px solid #03111f;}
-		        table tr{ border:1px solid gray;}
-		        table td, table th{padding:0px 10px; border: 1px solid gray;}
-			</style>';
-		$info .= '<table>
-			<tbody>
-			<tr><td style="text-align: center;" colspan="2"><strong>Info Coche</strong></td></tr>
-			<tr><td><strong>Localizador</strong></td><td>'.$loc.'</td></tr>
-			<tr><td><strong>Fecha Venta</strong></td><td>'.$f_venta.'</td></tr>
-			<tr><td><strong>Recoge Coche</strong></td><td>'.$f_entrega.'</td></tr>
-			<tr><td><strong>Entrega coche</strong></td><td>'.$f_recogida.'</td></tr>
-			<tr><td><strong>Cantidad d&iacute;as</strong></td><td>'.$c_dias.'</td></tr>
-			<tr><td><strong>Telefono</strong></td><td>'.$telefono.'</td></tr>
-			<tr><td><strong>Contacto</strong></td><td>'.$contacto.'</td></tr>
-			<tr><td><strong>Nombre</strong></td><td>'.$nombre.'</td></tr>
-			</tbody>
-			</table>';
-		$this->info = $info;
-	
-		$headers = "MIME-Version: 1.0" . "\r\n";
-		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-		$headers .= 'From: <ventas@jyctel.com>' . "\r\n";
-		//$headers .= 'Cc: diego@jyctel.com' . "\r\n";
-		
-		mail( $this->getEmails ('coche'), 'Resumen Venta Coche - '.$this->token, $this->info,$headers);
-	}
-
-	public function sendEmailTrustPilot($venta, $producto, $asignaciones_coche) {
-		
-		$fecha_pago = date('Y-m-d');
-		foreach ($producto as $bloque) {
-			if (isset($bloque['producto'])) {
-				$fecha_pago = $bloque['producto']['fecha_venta'];
-			}
-			if (isset($bloque['facturacion'])) {
-				$cliente = $bloque['facturacion'];
-			} else{
-				$cliente = $bloque['cliente'];
-			}
-		}
-		
-		$cantidad = 0;	
-		foreach ($asignaciones_coche as $vals) {
-			$divisa = ($vals['divisa'] == 1 ? '&euro;' : '$');
-			$cantidad += $vals['cantidad'];
-		}	
-
-		$iNombre = explode(' ', $cliente['nombre']);
-		$nombre = ucfirst($iNombre[0]);
-		$nombre_completo = ucfirst($cliente['nombre']);
-
-		$vars_plantilla = array(
-			'[nombre]'=> $nombre,
-			'[nombre-completo]'=> $nombre_completo,
-			'[fecha-pago]' => $fecha_pago,
-			'[precio]' => number_format( $cantidad, 2, ',' , '') . ' ' . $divisa,
-		);
-		
-		$archivo_ori = file_get_contents('/var/www/html/factura/contratos-ventas/plantilla-cubarentcars/index.html');
-		$archivo = $archivo_ori;
-		foreach ($vars_plantilla as $tag => $value) {
-
-	 	       $archivo = str_replace( $tag, $value, $archivo);
-		}
-
-		$email_trustpilot = 'cubarentcars.com+399c370bbc@invite.trustpilot.com';		
-	
-		$headers = "MIME-Version: 1.0" . "\r\n";
-		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-		$headers .= 'From: <ventas@jyctel.com>' . "\r\n";
-		$headers .= 'Bcc: '.$email_trustpilot . "\r\n";
-
-		$subject = 'Confirmacion de pago';
-		$email_cliente = $cliente['email'];	
-		//$email_cliente = 'thiegui@yahoo.es';	
-		
-		$res = mail($email_cliente, $subject, $archivo, $headers);
-
-		syslog (LOG_INFO, __FILE__ . ':'.__METHOD__ . ':cantidad: ' . $cantidad);
-		syslog (LOG_INFO, __FILE__ . ':'.__METHOD__ . ':' . ($res ? 'sended -- '.$email_cliente:'not-sended ' . $email_cliente));
-		
-	}
-
-	function send(){
-		$headers = "MIME-Version: 1.0" . "\r\n";
-		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-		$headers .= 'From: <ventas@jyctel.com>' . "\r\n";
-		//lias$headers .= 'Cc: diego@jyctel.com' . "\r\n";
-		
-		mail( $this->getEmails(), 'Resumen Venta - '.$this->token . $this->hay_efectivo, $this->info,$headers);
 	}
 }
